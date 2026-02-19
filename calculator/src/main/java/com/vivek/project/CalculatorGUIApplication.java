a3220b2e1fa967776f7ef6b67c169828bc6ab5fd
Vivek Shelke <vivekshelke12jul@gmail.com>
2026-02-19 17:51:52 +0530

    calculator: project init

---
+++ calculator/src/main/java/com/vivek/project/CalculatorGUIApplication.java
@@ -0,0 +1,8 @@
+package com.vivek.project;
+
+public class CalculatorGUIApplication {
+
+    public CalculatorGUIApplication(){
+        System.out.println("this will be a calculator GUI Application");
+    }
+}
