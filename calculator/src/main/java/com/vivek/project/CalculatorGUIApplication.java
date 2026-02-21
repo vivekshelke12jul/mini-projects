c96cfc80b5f1c7132cdccb8349898105a4a1f01c
Vivek Shelke <vivekshelke12jul@gmail.com>
2026-02-21 15:51:21 +0530

    calculator: ui created without functionality

--- calculator/src/main/java/com/vivek/project/CalculatorGUIApplication.java
+++ calculator/src/main/java/com/vivek/project/CalculatorGUIApplication.java
@@ -1,9 +1,58 @@
 package com.vivek.project;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class CalculatorGUIApplication {
 
     public CalculatorGUIApplication(){
-//        System.out.println("this will be a calculator GUI Application");
-        new KFrame();
+
+        // Display Panel
+        JPanel dp = new JPanel();
+        dp.setPreferredSize(new Dimension(500, 100));
+        dp.setBackground(Color.LIGHT_GRAY);
+        dp.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
+        dp.setLayout(new BorderLayout());
+
+        JTextField display = new JTextField();
+        display.setFont(new Font("Segoe UI", Font.PLAIN, 50));
+        display.setHorizontalAlignment(SwingConstants.RIGHT);
+
+        dp.add(display, BorderLayout.CENTER);
+
+        // Buttons Panel
+        JPanel bp = new JPanel();
+
+        bp.setPreferredSize(new Dimension(100, 500));
+        bp.setBackground(Color.LIGHT_GRAY);
+        bp.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
+        bp.setLayout(new GridLayout(5,4, 5,5));
+
+        String[] buttonText = new String[]{
+                "( )", ")", "C", "<=",
+                "1", "2", "3", "/",
+                "4", "5", "6", "*",
+                "7", "8", "9", "-",
+                ".", "0", "=", "+",
+
+        };
+
+        for(String text: buttonText){
+            JButton button = new JButton(text);
+            button.setFont(new Font("Segoe UI", Font.PLAIN, 50));
+            bp.add(button);
+        }
+
+        // JFrame
+        JFrame frame = new JFrame();
+        frame.setSize(new Dimension(500, 600));
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+
+        frame.add(dp, BorderLayout.NORTH);
+        frame.add(bp, BorderLayout.CENTER);
+
+        frame.setVisible(true);
     }
 }
